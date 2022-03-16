555100f76e7b1c27ebedb7c1bab8d2c82a00d816
Morgan Berger <morgan.berger71@gmail.com>
2022-03-16 16:54:24 +0000

    Pushing empty pod lib

---
+++ Example/Pods/Target Support Files/LocoApiLib/LocoApiLib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LocoApiLib : NSObject
+@end
+@implementation PodsDummy_LocoApiLib
+@end
---
+++ Example/Pods/Target Support Files/Pods-LocoApiLib_Tests/Pods-LocoApiLib_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LocoApiLib_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_LocoApiLib_Tests
+@end
